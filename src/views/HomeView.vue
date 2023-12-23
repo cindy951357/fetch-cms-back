dd9ee4bb1f3f6f4ab277830eb97339844ec179a0
Cindy Chang <42115114+cindy951357@users.noreply.github.com>
2023-12-23 12:00:25 +0800

    compare reactive and ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,43 @@
 <template>
   <div class="home">
-    Home
-    <p ref="paragraph">
-      Name: {{ name }}
-      Age: {{ age }}
-    </p>
-    <button @click="handleClick">Click me!</button>
+    <h1>Home</h1>
+    <h2>Ref and Reactive</h2>
+    <h3>Ref</h3>
+    <p>Name: {{ ninjaOne.name }}</p>
+    <p>Age: {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Click me!</button>
+
+    <h3>Reactive</h3>
+    <p>Name: {{ ninjaTwo.name }}</p>
+    <p>Age: {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update Ninja Two</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { ref, } from 'vue';
+import { ref, reactive, } from 'vue';
 
 export default {
   name: 'HomeView',
   setup() {
-    // const paragraph = ref(null);
+    const ninjaOne = ref({name: 'mario', age: 30});
+    const ninjaTwo = reactive({ name: 'luigi', age: 35})
 
-    const name = ref('mario');
-    const age = ref(30);
 
-    const handleClick = () => {
-      name.value = 'luigi';
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40;
+    }
+
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 50;
     }
 
     return {
-      name,
-      age,
-      handleClick,
+      ninjaOne,
+      updateNinjaOne,
+      ninjaTwo,
+      updateNinjaTwo,
     };
   },
 }
